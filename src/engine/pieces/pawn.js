14ef7febf6402d833a43b55abdcdc054f7a43228
Shalini <shalini.dhamija@gmail.com>
2024-01-24 10:34:57 +0000

    Cherry-Pick 18 - More Pawn moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,86 +1,97 @@
-import Player from '../player';
-import Square from '../square';
-import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
+import King from "./king";
+import Piece from "./piece";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
-    getAvailableMoves(board) {
-      const location = board.findPiece(this);
+  constructor(player) {
+    super(player);
+  }
 
-      const getTwoMoves = () => {
-        let firstMove, secondMove;
-        if (this.player === Player.WHITE) {
-          firstMove = Square.at(location.row + 1, location.col);
-          secondMove = Square.at(location.row + 2, location.col);
-        } else {
-          firstMove = Square.at(location.row - 1, location.col);
-          secondMove = Square.at(location.row - 2, location.col);
-        }
-        //cannot move if there is a piece in front
-        return board.getPiece(firstMove) !== undefined
-          ? []
-          : [firstMove, secondMove];
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+
+    //Check if the pawns have reached their respective ends on the board
+    // i.e, for Whites = 7 and for Blacks = 0
+    if (
+      (location.row === 7 && this.player === Player.WHITE) ||
+      (location.row === 0 && this.player === Player.BLACK)
+    )
+      return [];
+
+    let pawnDirections;
+    if (this.player === Player.WHITE) {
+      pawnDirections = {
+        dright: { row: 1, col: 1 },
+        dleft: { row: 1, col: -1 },
+        frontOneStep: { row: 1, col: 0 },
+        frontTwoSteps: { row: 2, col: 0 },
       };
-      
-      const getOneMove = () => {
-        return this.player === Player.WHITE
-          ? [Square.at(location.row + 1, location.col)]
-          : [Square.at(location.row - 1, location.col)];
+    } else {
+      pawnDirections = {
+        dright: { row: -1, col: 1 },
+        dleft: { row: -1, col: -1 },
+        frontOneStep: { row: -1, col: 0 },
+        frontTwoSteps: { row: -2, col: 0 },
       };
-  
-      //Pawn can move one or two spaces if it is their first move i.e., at the initial position
-      // initial position for the Wite pawns is row=1
-      // initial position for the Black pawns is row=6
-      const pawnMoves =
-        (location.row === 1 && this.player === Player.WHITE) ||
-        (location.row === 6 && this.player === Player.BLACK)
-          ? getTwoMoves()
-          : getOneMove();
-      
-      const availableMoves = pawnMoves.filter(
-        (loc) => board.getPiece(loc) === undefined
-      );
-      
-      return availableMoves;
     }
+    const getSquareSpace = (direction) => {
+      const newRow = location.row + direction.row;
+      const newCol = location.col + direction.col;
+      return [Square.at(newRow, newCol)];
+    };
+    // Pawn can move diagonally in two directions if it is between col 1-6
+    // for col 0 it can only move to its diagonal right
+    //for col 1 it can only move  to its diagonal left
+    let diagonalMoves;
+    if (location.col === 0)
+      diagonalMoves = getSquareSpace(pawnDirections.dright);
+    else if (location === 7)
+      diagonalMoves = getSquareSpace(pawnDirections.dleft);
+    else
+      diagonalMoves = [
+        ...getSquareSpace(pawnDirections.dright),
+        ...getSquareSpace(pawnDirections.dleft),
+      ];
 
+    const opponentPeice =
+      this.player === Player.WHITE ? Player.BLACK : Player.WHITE;
+    const canMakeDiagonalMove = (loc) => {
+      const peice = board.getPiece(loc);
+      if (
+        peice !== undefined &&
+        peice.player === opponentPeice &&
+        !(peice instanceof King)
+      ) {
+        return peice;
+      }
+    };
+    const validDiagonalMoves = diagonalMoves.filter((loc) =>
+      canMakeDiagonalMove(loc)
+    );
 
+    if (validDiagonalMoves.length !== 0) return validDiagonalMoves;
 
+    const getTwoMoves = () => {
+      const firstMove = getSquareSpace(pawnDirections.frontOneStep);
+      return board.getPiece(...firstMove) !== undefined
+        ? []
+        : [...firstMove, ...getSquareSpace(pawnDirections.frontTwoSteps)];
+    };
+
+    //Pawn can move one or two spaces if it is their first move i.e., at the initial position
+    // initial position for the Wite pawns is row=1
+    // initial position for the Black pawns is row=6
+    const pawnForwardMoves =
+      location.row === 1 || location.row === 6
+        ? getTwoMoves()
+        : getSquareSpace(pawnDirections.frontOneStep);
+
+    // Check if the square is empty, if it is then it is a valid move
+    const availableMoves = pawnForwardMoves.filter(
+      (loc) => board.getPiece(loc) === undefined
+    );
+    return availableMoves;
+  }
 }
 
-
-/*Pawn
-    white pawns
-      1) can only move one square up if they have already moved
-      2) can move one or two squares up on their first move
-    black pawns
-      3) can only move one square down if they have already moved
-      4) can move one or two squares down on their first move
-    5)cannot move if there is a piece in front
-      */
-    // getAvailableMoves(board) {
-    //     let location = board.findPiece(this)
-        
-    //     const getTwoMoves = () => {
-    //        return this.player === Player.WHITE
-    //           ? [Square.at(location.row + 1, location.col),Square.at(location.row + 2, location.col)]
-    //           : [Square.at(location.row - 1, location.col),Square.at(location.row - 2, location.col)];
-    //       };
-
-    //       const getOneMove = (player) => {
-    //        return this.player === Player.WHITE
-    //           ? [Square.at(location.row + 1, location.col)]
-    //           : [Square.at(location.row - 1, location.col)];
-    //       };
-
-          
-          
-    //     //Initial position for the white pawns is second row from bottom i.e. row = 1
-    //     //Initial position for the black pawns is second row from top i.e. row = 6
-    //     if (location.row === 1 || location.row === 6) return getTwoMoves();
-    //     else return getOneMove();
-
-    //     //cannot move if there is a piece in front
-    // }
