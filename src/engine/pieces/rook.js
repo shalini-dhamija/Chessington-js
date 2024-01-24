4c8b461dbf9a219bb0661d434057e7fd558acdf1
Shalini <shalini.dhamija@gmail.com>
2024-01-24 10:49:02 +0000

    Cherry-Pick 9 - More Rook Moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -13,17 +13,47 @@ export default class Rook extends Piece {
         const location = board.findPiece(this);
         let arrExpectedMoves = [];
 
-        
-        for(let i=0; i<GameSettings.BOARD_SIZE;i++){
-            //Get Horizontal Moves
-            if(i!==location.col){
+        //Get Horizontal Forward Moves
+        for(let i=location.col+1;i<GameSettings.BOARD_SIZE;i++){
+            if(board.getPiece(Square.at(location.row,i))===undefined)
                 arrExpectedMoves.push(Square.at(location.row, i));
-            }
-            //Get Vertical Moves
-            if(i!==location.row){
+            else
+                break;
+        }
+        //Get Horizontal Backward Moves
+        for(let i=location.col-1;i>=0;i--){
+            if(board.getPiece(Square.at(location.row,i))===undefined)
+                arrExpectedMoves.push(Square.at(location.row, i));
+            else
+                break;
+        }
+        //Get Vertical Forward Moves
+        for(let i=location.row+1;i<GameSettings.BOARD_SIZE;i++){
+            if(board.getPiece(Square.at(i,location.col))===undefined)
                 arrExpectedMoves.push(Square.at(i, location.col));
-            }
+            else
+                break;
+        }
+        //Get Vertical Backward Moves
+        for(let i=location.row-1;i>=0;i--){
+            if(board.getPiece(Square.at(i,location.col))===undefined)
+            arrExpectedMoves.push(Square.at(i, location.col));
+            else
+                break;
         }
         return arrExpectedMoves;
-    }
+    }    
 }
+
+
+// for(let i=0; i<GameSettings.BOARD_SIZE;i++){
+        //     //Get Horizontal Moves
+        //     if(i!==location.col){
+        //         arrExpectedMoves.push(Square.at(location.row, i));
+        //     }
+        //     //Get Vertical Moves
+        //     if(i!==location.row){
+        //         arrExpectedMoves.push(Square.at(i, location.col));
+        //     }
+        // }
+        //return arrExpectedMoves;
